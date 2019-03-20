a59edccee9707c65a8bc852e2d75544332407e40
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD13119.corp.capgemini.com>
2019-03-20 10:59:56 +0530

    userbean updation by sayan 20 march2019

--- CapbookStore/src/main/java/com/cg/capbook/beans/User.java
+++ CapbookStore/src/main/java/com/cg/capbook/beans/User.java
@@ -2,14 +2,22 @@ package com.cg.capbook.beans;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class User {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="use_gen")
+	@SequenceGenerator(name="use_gen", sequenceName="use_seq",allocationSize=1,initialValue=110)
 	private int userId;
+	@NotEmpty
 	private String firstName;
+	@NotEmpty
 	private String lastName;
+	private String dob;
+	private long mobileNo;
 	
 }
